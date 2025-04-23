f6e452777504af53b73c2dffc8237cfeb43472db
oreo <88177661+superoreoooooo@users.noreply.github.com>
2025-04-23 14:42:18 +0900

    제발시험잘보게해주세요

--- ImageProcessing/beforeTest.py
+++ ImageProcessing/beforeTest.py
@@ -4,7 +4,7 @@ import cv2
 import os
 
 # 4
-"""
+
 print(os.getcwd())
 a = cv2.imread("ImageProcessing/images_chap4/read_gray.jpg", cv2.IMREAD_GRAYSCALE)
 cv2.imshow("a", a)
@@ -12,9 +12,9 @@ b = cv2.waitKey(0)
 print(b)
 cv2.destroyAllWindows()
 
-"""
 
-"""
+
+
 def onMouse(event, x, y, flags, param) :
     if (event == cv2.EVENT_LBUTTONDOWN) :
         print("left click")
@@ -27,9 +27,9 @@ cv2.setMouseCallback("a", onMouse)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-"""
 
-"""
+
+
 img = np.full((300, 300, 3), 255, np.uint8)
 cv2.line(img, (50, 50), (250, 250), (255, 0, 0), 3)
 cv2.line(img, (250, 50), (50, 250), (0, 0, 255), 3)
@@ -37,9 +37,9 @@ cv2.imshow("a", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-"""
 
-"""
+
+
 img = np.full((1000, 1000, 3), 255, np.uint8)
 
 sPos = tuple()
@@ -60,9 +60,9 @@ cv2.setMouseCallback("a", onMouse)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-"""
 
-"""
+
+
 capture = cv2.VideoCapture(0)
 
 while True :
@@ -73,10 +73,10 @@ while True :
     cv2.imshow("a", frame)
     
 capture.release()
-"""
+
 
 # 5
-"""
+
 img = cv2.imread("test.png")
 img = cv2.flip(img, -1)
 #img = cv2.repeat(img, 2, 3)
@@ -88,9 +88,9 @@ img = cv2.transpose(img)
 cv2.imshow("a", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-"""
 
-""" #split 후 merge로 한색만뜨게하기
+
+ #split 후 merge로 한색만뜨게하기
 img = cv2.imread("ImageProcessing/images_chap5/color.jpg")
 img = cv2.split(img)
 
@@ -103,9 +103,9 @@ print(img)
 cv2.imshow("img", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-"""
 
-"""
+
+
 img = cv2.imread("ImageProcessing/images_chap5/bit_test.jpg", cv2.IMREAD_COLOR)
 logo = cv2.imread("ImageProcessing/images_chap5/logo.jpg", cv2.IMREAD_COLOR)
 
@@ -130,9 +130,9 @@ img[y : y + h, x : x + w] = dst # 원본 이미지에 적용
 cv2.imshow("a", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-"""
 
-"""
+
+
 img1 = cv2.imread("ImageProcessing/images_chap5/abs_test1.jpg", cv2.IMREAD_GRAYSCALE)
 img2 = cv2.imread("ImageProcessing/images_chap5/abs_test2.jpg", cv2.IMREAD_GRAYSCALE)
 
@@ -145,9 +145,9 @@ titles = ['img1', 'img2', 'difimg1', 'absdif1', 'absdif2']
 for t in titles :
     cv2.imshow(t, eval(t))
 cv2.waitKey(0)
-"""
 
-"""
+
+
 img = cv2.imread("ImageProcessing/images_chap5/minMax.jpg", cv2.IMREAD_GRAYSCALE)
 
 min_val, max_val, _, _ = cv2.minMaxLoc(img)
@@ -161,9 +161,9 @@ print(min_val, max_val)
 cv2.imshow("img", img)
 cv2.imshow("dst", dst)
 cv2.waitKey(0)
-"""
 
-"""
+
+
 img = cv2.imread("ImageProcessing/images_chap5/sum_test.jpg", cv2.IMREAD_COLOR)
 
 sum = cv2.sumElems(img)
@@ -182,10 +182,10 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 cv2.imshow("A piece of modern art", img)
 cv2.waitKey(0)
-"""
+
 
 # 6
-"""
+
 img1 = cv2.imread("ImageProcessing/images_chap6/add1.jpg", cv2.IMREAD_GRAYSCALE)
 img2 = cv2.imread("ImageProcessing/images_chap6/add2.jpg", cv2.IMREAD_GRAYSCALE)
 
@@ -202,4 +202,88 @@ for t in titles :
     
 cv2.waitKey()
 
-"""
+
+
+
+def avgFilter(img, ksize=5) :
+    r, c = img.shape[:2]
+    dst = np.zeros((r, c), np.uint8)
+    center = ksize // 2
+    
+    for i in range(r) :
+        for j in range(c) :
+            y1, y2 = i - center, i + center + 1
+            x1, x2 = j - center, j + center + 1
+            
+            if (y1 < 0 or y2 > r or x1 < 0 or x2 > c) : dst[i, j] = img[i, j]
+            else : 
+                mask = img[y1 : y2, x1 : x2]
+                dst[i, j] = cv2.mean(mask)[0]
+    
+    return dst
+
+img = cv2.imread("ImageProcessing/images_chap6/add1.jpg", cv2.IMREAD_GRAYSCALE)
+
+avgImg = avgFilter(img, 5)
+#blurImg = cv2.blur(img, (5, 5), (-1, -1), cv2.BORDER_REFLECT) # why error ?
+boxImg = cv2.boxFilter(img, ddepth=-1, ksize=(5, 5))
+
+cv2.imshow("img", img)
+cv2.imshow("avg", avgImg)
+#cv2.imshow("blur", blurImg)
+cv2.imshow("box", boxImg)
+
+cv2.waitKey(0)
+
+
+
+
+def onTrackbar(threshold) :
+    edge = cv2.GaussianBlur(img, (5, 5), 0)
+    edge = cv2.Canny(edge, threshold, threshold * 2, 5)
+    
+    color_edge = cv2.copyTo(img, mask=edge)
+    dst = cv2.hconcat([img, color_edge])
+    
+    cv2.imshow("color edge", dst)
+    
+img = cv2.imread("ImageProcessing/images_chap7/color_edge.jpg", cv2.IMREAD_COLOR)
+
+threshold = 50
+repImg = cv2.repeat(img, 1, 2)
+repGray = cv2.cvtColor(repImg, cv2.COLOR_BGR2GRAY)
+cv2.namedWindow("color edge", cv2.WINDOW_AUTOSIZE)
+cv2.createTrackbar("Canny", "color edge", threshold, 100, onTrackbar)
+onTrackbar(threshold)
+cv2.waitKey(0)
+
+
+
+
+img = img = cv2.imread("ImageProcessing/images_chap7/morph.jpg", cv2.IMREAD_GRAYSCALE)
+
+#data = [0, 1, 0, 1, 1, 1, 0, 1, 0]
+data = [0, 1, 0, 1, 1, 1, 0, 1, 0]
+
+mask = np.array(data, np.uint8).reshape(3, 3)
+
+th_img = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY)[1]
+
+ #침식
+dst1 = cv2.erode(th_img, mask)
+dst2 = cv2.morphologyEx(th_img, cv2.MORPH_ERODE, mask)
+
+
+ #팽창
+dst1 = cv2.dilate(th_img, mask)
+dst2 = cv2.morphologyEx(th_img, cv2.MORPH_DILATE, mask)
+
+
+dst1 = cv2.morphologyEx(th_img, cv2.MORPH_OPEN, mask)
+dst2 = cv2.morphologyEx(th_img, cv2.MORPH_CLOSE, mask)
+
+cv2.imshow("img", img)
+cv2.imshow("dst1", dst1)
+cv2.imshow("dst2", dst2)
+
+cv2.waitKey(0)
