d74bbad3c85a0f3d7cb128fc5c2eea438534b331
oreo <88177661+superoreoooooo@users.noreply.github.com>
2025-05-09 14:12:42 +0900

    a

---
+++ windowsProgramming/p0509/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace p0509;
+
+class Program
+{
+    public delegate void TestDelegate();
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        for (int i = 0; i < 10; i++) {
+            Thread tr = new Thread(() => {
+                Console.WriteLine("ASDF");
+                try {
+                    Thread.Sleep(1000);
+                } catch(Exception e) {
+                    
+                }
+                Console.WriteLine("FDSA");
+            });
+            tr.Start();
+        }
+    }
+}
